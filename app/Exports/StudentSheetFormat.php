edae0e35a0fa19482f27ac990a1d6083fad30199
Mei.dy <meidyfachreza@gmail.com>
2021-06-06 23:34:49 +0800

    setting excel format

--- app/Exports/StudentSheetFormat.php
+++ app/Exports/StudentSheetFormat.php
@@ -4,11 +4,13 @@ namespace App\Exports;
 use App\Models\Consumer;
 use App\Models\Employee;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
 
-class StudentSheetFormat implements WithTitle, WithHeadings
+class StudentSheetFormat implements WithTitle, WithHeadings, FromArray
 {
 
     protected $schoolId;
@@ -16,6 +18,19 @@ class StudentSheetFormat implements WithTitle, WithHeadings
     public function __construct($schoolId = Null)
     {
         $this->schoolId = $schoolId;
+    }
+
+    public function array(): array
+    {
+        if ($this->schoolId==0) {
+            return [
+                ["0044959620", "ABIDAH IZDIHAR DZAKIYYAH", "BONTANG", "MM/DD/YYYY", "Islam", "P", "Wasikin", "", "8 (silahkan lihat dari sheet Id Sekolah)", 2012, "2010/2011", "DN-16/D-SMP/13/2106457"],
+            ];
+        }else {
+            return [
+                ["0044959620", "ABIDAH IZDIHAR DZAKIYYAH", "BONTANG", "MM/DD/YYYY", "Islam", "P", "Wasikin", "", 2012, "2010/2011", "DN-16/D-SMP/13/2106457"],
+            ];
+        }
     }
 
     public function headings(): array
