16b2efd753c1ef2b4866acdcd73219bfbb3d7a41
Mei.dy <meidyfachreza@gmail.com>
2021-10-16 22:36:50 +0800

    update

--- app/Models/IncomingLetter.php
+++ app/Models/IncomingLetter.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Carbon;
+// use Illuminate\Support\Carbon;
 
 class IncomingLetter extends Model
 {
@@ -14,11 +14,11 @@ class IncomingLetter extends Model
 
     public function setDateAttribute($value)
     {
-        $this->attributes['date'] = (new Carbon($value))->format('Y-m-d');
+        $this->attributes['date'] = date('Y-m-d', strtotime($value));
     }
 
     public function getDateAttribute($value)
     {
-        return $this->attributes['date'] = (new Carbon($value))->format('Y-m-d');
+        return $this->attributes['date'] = date('Y-m-d', strtotime($value));
     }
 }
